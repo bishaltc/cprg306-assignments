ed772f8c93f71bd0f4b0849588ec71d20dd774e5
bishaltc <bishal.thapachhetri@edu.sait.ca>
2024-09-17 21:23:00 -0600

    week 2 assignment
     made new foder called week-2 ,  create file as page.js  for displaing the work of week 2 and  also create student-info under same folder
     for creating a component called StudentInfo

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,11 @@
+import StudentInfo from "./student-info";
+
 export default function page() {
     return (
         <main>
+     <h1> Shopping lists </h1>
 
+     <StudentInfo />
         </main>
     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <h2> Bishal Thapa Chhetri</h2>
+
+            <Link href = "https://github.com/bishaltc/cprg306-assignments"> My Github link </Link>
+        </div>
+    );
+
+
+}
